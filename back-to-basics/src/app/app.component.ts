62063a047b88ad789ae0c34ee96127e7cb474276
Pedro Ramon <pedrorcdiaz5@gmail.com>
2025-04-14 21:11:11 -0300

    primeiras paginas

--- back-to-basics/src/app/app.component.ts
+++ back-to-basics/src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HobbiesComponent } from './components/hobbies/hobbies.component';
+import { HomeComponent } from './components/home/home.component';
+import { SportComponent } from './components/sport/sport.component';
+import { WorkComponent } from './components/work/work.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+     CommonModule,
+     RouterOutlet,
+     HobbiesComponent,
+     HomeComponent, 
+     SportComponent, 
+     WorkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'back-to-basics';
+  title = 'Portifolio';
 }
